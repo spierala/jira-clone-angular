326425d5996a6c218442af9c7d0e1c369c5b83cc
Florian Spier <mail@florian-spier.de>
2021-04-10 08:17:33 +0200

    Refactor to mini-rx

--- frontend/src/app/project/state/project/project.query.ts
+++ frontend/src/app/project/state/project/project.query.ts
@@ -1,24 +1,24 @@
-import { ProjectState, ProjectStore } from './project.store';
+import { ProjectStore } from './project.store';
 import { Injectable } from '@angular/core';
-import { Query } from '@datorama/akita';
 import { IssueStatus, JIssue } from '@trungk18/interface/issue';
-import { map, delay } from 'rxjs/operators';
+import { delay, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
-export class ProjectQuery extends Query<ProjectState> {
-  isLoading$ = this.selectLoading();
-  all$ = this.select();
-  issues$ = this.select('issues');
-  users$ = this.select('users');
+export class ProjectQuery {
+  isLoading$ = this.store.select(state => state.isLoading);
+  all$ = this.store.select();
+  issues$ = this.store.select(state => state.issues);
+  users$ = this.store.select(state => state.users);
 
   constructor(protected store: ProjectStore) {
-    super(store);
+
   }
 
   lastIssuePosition = (status: IssueStatus): number => {
-    const raw = this.store.getValue();
+    const raw = this.store.state;
     const issuesByStatus = raw.issues.filter(x => x.status === status);
     return issuesByStatus.length;
   };
